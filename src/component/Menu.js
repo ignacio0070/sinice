31dab56438f4f10ebed7276499ffb4ecd4146c32
ignacio0070 <ignaciocaceres123@hotmail.com>
2021-12-19 21:49:28 -0300

    contrui el menu de la pagina

---
+++ src/component/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Menu() {
+    return (
+        
+            <nav className='menu'>
+                <label className='logo' >Logo</label>
+                <ul className='menu_items'>
+                
+                    <li><a href=''> Inicio</a></li>
+                    <li> <a href=''>Repuestos</a></li>
+                    <li> <a href=''>Bicicletas</a></li>
+               
+                </ul>
+              
+                
+            </nav>
+      
+            
+      
+    )
+}
+
+export default Menu
